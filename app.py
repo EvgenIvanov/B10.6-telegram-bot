9255a84d1c4b08335e521f868ceff0f6b3d1b0d9
EvgenIvanov <evgen2007@bk.ru>
2021-01-13 23:51:38 +0300

    wrote a method handler_text, refactoring

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import telebot
-import json
-import requests
-from config import currency, TOKEN
+from extensions import CustomException, ExchangeRates
+import config as conf
 
-bot = telebot.TeleBot(TOKEN)
-
-class CustomException(Exception):
-    pass
-
-class ExchangeRates():
-    pass
+bot = telebot.TeleBot(conf.TOKEN)
 
 @bot.message_handler(commands=['start','help'])
 def handler_start_help(message: telebot.types.Message):
@@ -22,18 +15,44 @@ def handler_start_help(message: telebot.types.Message):
                 str_ = message.chat.first_name
             if message.chat.last_name and isinstance(message.chat.last_name, str):
                 str_ += ' ' + message.chat.last_name
-        str_ += ', Вас приветствует бот\n\n'
+        str_ += ', Вас приветствует бот\n\nЯ умею конвертировать валюты\n\n'
 
-    str_ +='Я умею конвертировать валюты\n\nДля начала необходимо ввести команду в формате:\n \
-<имя валюты> <в какую валюту перевести> <количество переводимой валюты>\n\nНаберите команду /currency, что бы увидеть список доступных валют.'
+    str_ +='Для начала необходимо ввести команду в формате:\n \
+<имеющаяся валюта> <нужная валюта> <количество нужной валюты>\n\n \
+Введите команду /help для отображения подсказки\n\n \
+Введите команду /values, что бы увидеть список доступных валют.'
     
     bot.reply_to(message, str_)
 
-@bot.message_handler(commands=['currency'])
+@bot.message_handler(commands=['values'])
 def handler_currency(message):
     str_ = 'Список доступных валют:'
-    for curr in currency:
+    for curr in conf.currency:
         str_ = '\n'.join((str_, curr)) # str_ += '\n' + curr
     bot.reply_to(message, str_)
+
+@bot.message_handler(content_types=['text'])
+def handler_text(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        
+        if len(values) != 3:
+            raise CustomException('Вы ввели неверное количество параметров!')
+        
+        base, quote, amount = values
+
+        total_amount = ExchangeRates.get_price(base, quote, amount)
+
+    except CustomException as tEx:
+        text = f'<b>Внимание!</b>\n{tEx}\n\nДля справки введите команду: /help'
+        bot.send_message(message.chat.id, text, parse_mode='HTML')
+
+    except Exception as tEx:
+        text = f'<b>Error!</b>\n{tEx}'
+        bot.send_message(message.chat.id, text, parse_mode='HTML')
+    
+    else:
+        result = f'для покупки {amount}{conf.currency[quote]} вам понадобится {str(total_amount)}{conf.currency[base]}'
+        bot.send_message(message.chat.id, result)
 
 bot.polling(none_stop = True)
